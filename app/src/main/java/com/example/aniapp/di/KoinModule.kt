904b2f15842d86cf307994b59a125dd92ae602a3
DanGugly <elijahmckoy1997@hotmail.com>
2022-01-07 21:04:48 -0500

    Update network call to Repo instead of VM

--- app/src/main/java/com/example/aniapp/di/KoinModule.kt
+++ app/src/main/java/com/example/aniapp/di/KoinModule.kt
@@ -1,14 +1,24 @@
 package com.example.aniapp.di
 
 import com.example.aniapp.rest.NekosBestAPI
+import com.example.aniapp.rest.NekosBestRepo
+import com.example.aniapp.rest.NekosBestRepoImpl
+import com.example.aniapp.viewmodel.NekoBestViewModel
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
+
+    fun provideNekoBestRepo(nekosBestAPI: NekosBestAPI) : NekosBestRepo = NekosBestRepoImpl(nekosBestAPI)
+
+    fun provideGson() = GsonBuilder().create()
 
     fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
 
@@ -20,17 +30,19 @@ val networkModule = module {
             .writeTimeout(30, TimeUnit.SECONDS)
             .build()
 
-    fun provideNekoBestAPI(okHttpClient: OkHttpClient) =
+    fun provideNekoBestAPI(okHttpClient: OkHttpClient, gson: Gson) =
         Retrofit.Builder()
             .baseUrl(NekosBestAPI.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(okHttpClient)
             .build()
             .create(NekosBestAPI::class.java)
 
+    single { provideGson() }
     single { provideLoggingInterceptor() }
     single { okHttpClient(get()) }
-    single { provideNekoBestAPI(get()) }
+    single { provideNekoBestAPI(get(), get()) }
+    single { provideNekoBestRepo(get()) }
 }
 
 val appModule = module {
@@ -38,5 +50,7 @@ val appModule = module {
 }
 
 val viewModelModule = module {
-    // viewModel {}
+    viewModel {
+        NekoBestViewModel(get())
+    }
 }
--- app/src/main/java/com/example/aniapp/viewmodel/NekoBestViewModel.kt
+++ app/src/main/java/com/example/aniapp/viewmodel/NekoBestViewModel.kt
@@ -3,47 +3,54 @@ package com.example.aniapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.aniapp.rest.NekosBestAPI
+import com.example.aniapp.rest.NekosBestRepo
 import com.example.aniapp.utils.UIState
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
 
 class NekoBestViewModel(
-    private val nekosBestAPI: NekosBestAPI,
+    private val nekosBestRepo: NekosBestRepo,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
-    private val coroutineScope : CoroutineScope = CoroutineScope(ioDispatcher)
-) : ViewModel() {
+    private val coroutineScope : CoroutineScope = CoroutineScope(SupervisorJob() + ioDispatcher)
+) : ViewModel(), CoroutineScope by coroutineScope {
     private var _allNekos : MutableLiveData<UIState> = MutableLiveData(UIState.LOADING())
     val allNekosObserver: LiveData<UIState> get() = _allNekos
 
     private var _allNekoGifs : MutableLiveData<UIState> = MutableLiveData(UIState.LOADING())
     val allNekoGifsObserver : LiveData<UIState> get() = _allNekoGifs
 
-    fun getNekos(){
-        coroutineScope.launch {
-            try {
-                val response = nekosBestAPI.getNekos()
-                response.body()?.let {
-                    _allNekos.postValue(UIState.SUCCESSNEKOS(listOf(it)))
-                } ?: _allNekos.postValue(UIState.ERROR(Throwable("Response is null")))
+    fun subscribeToNekos(){
+        launch{
+            nekosBestRepo.getNekos()
+        }
+    }
 
-            } catch (e : Exception){
-                _allNekos.postValue(UIState.ERROR(e))
+    fun subscribeToNekoGifs() {
+        launch{
+            nekosBestRepo.getNekoGifs()
+        }
+    }
+
+    private fun collectNekosData(){
+        launch {
+            nekosBestRepo.nekos.collect{ uiState ->
+                when(uiState){
+                    is UIState.LOADING -> { _allNekos.postValue(uiState) }
+                    is UIState.SUCCESSNEKOS -> { _allNekos.postValue(uiState) }
+                    is UIState.ERROR -> { _allNekos.postValue(uiState) }
+                }
             }
         }
     }
 
-    fun getNekoGifs(){
-        coroutineScope.launch {
-            try {
-                val response = nekosBestAPI.getGifs()
-                response.body()?.let {
-                    _allNekoGifs.postValue(UIState.SUCCESSNEKOSGIF(listOf(it)))
-                } ?: _allNekoGifs.postValue(UIState.ERROR(Throwable("Response is null")))
-            } catch (e : Exception){
-                _allNekoGifs.postValue(UIState.ERROR(e))
+    private fun collectNekoGifsData(){
+        launch {
+            nekosBestRepo.nekosGifs.collect{ uiState ->
+                when(uiState){
+                    is UIState.LOADING -> { _allNekoGifs.postValue(uiState) }
+                    is UIState.SUCCESSNEKOSGIF -> { _allNekoGifs.postValue(uiState) }
+                    is UIState.ERROR -> { _allNekoGifs.postValue(uiState) }
+                }
             }
         }
     }
